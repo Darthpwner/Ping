1df92cd11149c4dfabf7b5a62e2feb6bd03e3c0b
Matthew Allen Lin <matthewallenlin@Matthews-MacBook-Pro.local>
2017-09-29 23:14:57 -0700

    Setup game loop

--- ping.js
+++ ping.js
@@ -5,3 +5,29 @@ function Game() {
 	this.context = canvas.getContext("2d");
 	this.context.fillStyle = "white";
 }
+
+Game.prototype.draw = function()
+{
+	this.context.clearRect(0, 0, this.width, this.height);
+	this.context.fillRect(this.width/2, 0, 10, this.height);
+};
+
+Game.prototype.update = function()
+{
+	if (this.paused)
+		return;
+};
+
+// Initialize our game instance
+var game = new Game();
+
+function MainLoop() {
+	game.update();
+	game.draw();
+
+	// Call the main loop again at a frame rate of 30fps
+	setTimeout(MainLoop, 33.3333);
+}
+
+// Start the game execution
+MainLoop();
